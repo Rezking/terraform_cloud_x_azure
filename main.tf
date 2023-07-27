fd49df07262c44ad0ea312c20e09a1294b1d05e7
Onanuga Oreoluwa <onagoreoluwa92@gmail.com>
2023-07-27 12:41:42 +0100

    change output ip

--- main.tf
+++ main.tf
@@ -70,9 +70,9 @@ resource "azurerm_linux_virtual_machine" "vm_resource" {
     sku       = "20_04-lts"
     version   = "latest"
   }
-
-  output "vm_public_ip" {
-    description = "The public ip address of the virtual machine created"
-    value       = azurerm_linux_virtual_machine.vm_resource[count.index].public_ip_address
-  }
 }
+
+output "vm_public_ip" {
+    description = "The public ip address of the virtual machine created"
+    value       = ["${azurerm_linux_virtual_machine.vm_resource.*.public_ip_address}"]
+  }
