b203113bb16e2a84e8616d5c2f28ab09d814687d
Onanuga Oreoluwa <onagoreoluwa92@gmail.com>
2023-08-01 11:44:21 +0100

    add network security group

--- nsg.tf
+++ nsg.tf
@@ -1,2 +1,24 @@
 resource "azurerm_network_security_group" "rez_nsg" {
+  name                = "rez_nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "rez_nsg" {
+  name                        = "Allow SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "0.0.0.0/0"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.rez_nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.Ore_subnet.id
+  network_security_group_id = azurerm_network_security_group.rez_nsg.id
 }
