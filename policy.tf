ceacbf44d18bec7d22d8b8ba532b85467b1fd2e3
Onanuga Oreoluwa <onagoreoluwa92@gmail.com>
2023-08-01 09:26:25 +0100

    edit policy assignment

--- policy.tf
+++ policy.tf
@@ -15,15 +15,14 @@ data "template_file" "allowed_locations_rule" {
     template = file("${path.module}/allowed_locations_rule.json.tpl")
     
     vars = {
-        allowed_locations = locals.allowed_locations_json
+        allowed_locations = local.allowed_locations_json
     }
 }
 
-resource "azurerm_policy_assignment" "assignment" {
+resource "azurerm_resource_group_policy_assignment" "assignment" {
   name                 = "allowed_location_assignment"
-  display_name         = "Assignment for locations"
+  resource_group_id = var.azurerm_resource_group.rg.id
   policy_definition_id = azurerm_policy_definition.allowed_locations.id
-  scope = "/subscriptions/${var.subscriptionId}/resourceGroups/${var.rg_name}"
 }
 
 output "policy_rule_generated" {
